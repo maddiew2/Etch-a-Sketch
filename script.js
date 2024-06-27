d300f8c3c36b46e4f8409285620e51407f9fa548
maddiew2 <webbmmw.2@gmail.com>
2024-06-27 18:22:03 -0500

    added shake button
    The shake button can be pressed to clear the board.

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+let DEFAULT_SIZE = 40;
+
+window.onload = () => {
+    setUpGrid(DEFAULT_SIZE);
+    const shakeButton = document.getElementById('shake-button');
+    shakeButton.addEventListener('click', clearGrid);
+}
+
+function setUpGrid(gridSize) {
+    let squareSizeW = 550 / gridSize;
+    let squareSizeH = 410 / gridSize;
+    const grid = document.querySelector('.grid');
+    
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        const square = document.createElement('div');
+        grid.appendChild(square);
+        square.style.width = squareSizeW + "px";
+        square.style.height = squareSizeH + "px";
+
+        square.addEventListener('mouseover', () => {
+            console.log("Square hovered");
+            square.style.backgroundColor = 'black';
+        });
+    }
+}
+
+function clearGrid() {
+    const squares = document.querySelectorAll('.grid div');
+    squares.forEach(square => {
+        square.style.backgroundColor = 'white';
+    });
+}
+
+
+
+
+
 
